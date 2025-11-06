6a756e78ee1d9b063be9411a944b7afc571213f8
Shidochiri <ruthwk2@gmail.com>
2025-11-06 15:08:37 +0300

    url correctons

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list'),
+    path('books/', views.list_books, name='list_books'),
     path('books_class/', views.Booklist.as_view(), name='book_list_class'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
