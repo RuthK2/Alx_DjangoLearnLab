44cfc1ad6e169fa759b813df7473380a427b1d93
Shidochiri <ruthwk2@gmail.com>
2025-11-08 16:10:25 +0300

    Role Based Access Control

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Book
 from .models import Library
@@ -41,3 +41,33 @@ def logout_view(request):
     from django.contrib.auth import logout
     logout(request)
     return redirect('login')
+
+def is_admin(user):
+    try:
+        return user.userprofile.role == 'Admin'
+    except:
+        return False
+
+def is_librarian(user):
+    try:
+        return user.userprofile.role == 'Librarian'
+    except:
+        return False
+
+def is_member(user):
+    try:
+        return user.userprofile.role == 'Member'
+    except:
+        return False
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
