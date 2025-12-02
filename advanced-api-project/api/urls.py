164e38dd32fc2cfaeeed4f5e67246744a54a7806
Shidochiri <ruthwk2@gmail.com>
2025-12-02 21:32:58 +0300

    urls updates

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetail.as_view(), name='book-detail'),
     path('books/create/', BookCreate.as_view(), name='book-create'),
+    path('books/update/', BookUpdate.as_view(), name='book-update-no-id'),
+    path('books/delete/', BookDelete.as_view(), name='book-delete-no-id'),
     path('books/<int:pk>/update/', BookUpdate.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', BookDelete.as_view(), name='book-delete'),
 ]
