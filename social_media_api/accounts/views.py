1aac5d0e4efaa90e2e1933f714c9ba4b05b954ed
Shidochiri <ruthwk2@gmail.com>
2025-12-10 22:36:24 +0300

    Minor fixes

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, permissions
 from django.contrib.auth import authenticate
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 from rest_framework.authtoken.models import Token
@@ -74,8 +74,12 @@ class UnfollowUserView(APIView):
         except CustomUser.DoesNotExist:
             return Response({'error': 'User not found'},
                             status=status.HTTP_404_NOT_FOUND)
-        
+
+class UserListView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
     
-
-
-
+    def get(self, request):
+        users = CustomUser.objects.all()
+        serializer = self.get_serializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
